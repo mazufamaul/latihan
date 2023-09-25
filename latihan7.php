18f12a4b9c7461641c8253fc9c06bc575187798c
mazufamaul <husenzoelva24@gmail.com>
2023-09-25 20:14:54 +0700

    membuat file latihan php

---
+++ latihan7.php
@@ -0,0 +1,12 @@
+<?php
+
+for($x= 1; $x <=10; $x++){
+    echo '<br> bilangan 1 sampai dengan 10'.$x;
+}
+
+echo '<br> ----- bilangan 10 sampai 1 -----';
+
+for($y=10; $y >= 1; $y--){
+    echo '<br> bilangan : '.$y;
+}
+?>
