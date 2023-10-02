ceb584cb1f1f82de007933795f4c270a2e6bd196
mazufamaul <husenzoelva24@gmail.com>
2023-10-02 22:40:49 +0700

    membuat latihan

---
+++ Kucing.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "Hewan.php";
+    class Kucing extends Hewan
+    {
+        public function bersuara()
+        {
+        echo "<br/> Suara kucing meooong";
+        }
+    }
+?>
